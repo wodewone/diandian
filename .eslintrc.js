173c268315cf6ad07218b6b67522b11523678690
liuzq <liuzuoquan@huobi.com>
2020-11-03 18:30:28 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    root: true,
+    parser: 'babel-eslint',
+    parserOptions: {
+        sourceType: 'module'
+    },
+    extends: 'standard',
+    settings: {
+        'html/html-extensions': ['.html', '.mpx'],  // consider .html and .mpx files as HTML
+    },
+    plugins: [
+        'html'
+    ],
+    globals: {
+        wx: true,
+        getApp: true,
+        App: true,
+        __mpx_mode__: true
+    },
+    rules: {
+        'indent': [
+            'error',
+            4,
+            { 'SwitchCase': 1 }
+        ],
+        camelcase: ['error', { 'allow': ['__mpx_mode__'] }]
+    },
+}
