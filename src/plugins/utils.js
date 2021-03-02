5637bf824e472fd8853b47280c7d9e13870b8507
wodewone <wodewone@sina.com>
2021-03-03 00:17:39 +0800

    chore: fix

---
+++ src/plugins/utils.js
@@ -0,0 +1,10 @@
+export const obj2str = (obj) => {
+    let str = ''
+    for (const item in obj) {
+        if (obj.hasOwnProperty(item)) {
+            str += (str ? str + '&' : '')
+            str += `${item}=${obj[item]}`
+        }
+    }
+    return str
+}
